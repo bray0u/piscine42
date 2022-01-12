3110d813c274ed88a6e992e54ca01478825e4d19
bray0u <brayousub@gmail.com>
2022-01-13 00:42:57 +0100

    simplify strrev

--- j03/ex07/ft_strrev.c
+++ j03/ex07/ft_strrev.c
@@ -1,34 +1,9 @@
-#include <unistd.h>
-
-int ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
-
-void ft_putstr(char *str)
-{
-    int len;
-
-    len = ft_strlen(str);
-    write(1, str, len);
-}
-
-char *ft_strrev(char *str)
-{
-    
-    return (str);
-}
+#include <stdio.h>
 
 void main()
 {
     char *str;
 
     str = ft_strrev("abcdefghiklmnopqrstuvwxyz");
-    ft_putstr(str);
-    write(1, "\n", 1);
+    prinf("%s\n",str);
 }
