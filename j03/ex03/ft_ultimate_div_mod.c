b382dd0483c3ec05a8644c17ec3e6a0a105d13ca
bray0u <brayousub@gmail.com>
2022-01-12 13:54:08 +0100

    fix ultimatte div mod

--- j03/ex03/ft_ultimate_div_mod.c
+++ j03/ex03/ft_ultimate_div_mod.c
@@ -8,7 +8,7 @@ int main()
     a = 12;
     b = 8;
     printf("test1 : a = 12, b = 8\n");
-    ft_div_mod(&a, &b);
+    ft_ultimate_div_mod(&a, &b);
     a == 1 && b == 4
         ? printf("✔️ Correct\n")
         : printf("❌ Wrong\n\tdiv : '%d', expected : '1'\n\tmod: '%d', expected: '4'\n", a, b);
@@ -16,7 +16,7 @@ int main()
     a = 42;
     b = 5;
     printf("test1 : a = 42, b = 5\n");
-    ft_div_mod(&a, &b);
+    ft_ultimate_div_mod(&a, &b);
     a == 8 && b == 2
         ? printf("✔️ Correct\n")
         : printf("❌ Wrong\n\ta : '%d', expected : '8'\n\tb: '%d', expected: '2'\n", a, b);
