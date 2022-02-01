355562ceda6445c853d3e589e45ec10eb3db64e1
bray0u <brayousub@gmail.com>
2022-02-01 13:35:31 +0100

    fix strrevc

--- j03/ex07/ft_strrev.c
+++ j03/ex07/ft_strrev.c
@@ -2,8 +2,20 @@
 
 void main()
 {
-    char *str;
+    char str[27];
+    char *res;
+    int i;
 
-    str = ft_strrev("abcdefghiklmnopqrstuvwxyz");
-    prinf("%s\n",str);
+    str[0] = 'a';
+    i = 1;
+    while (str[i - 1] != 'z')
+    {
+        str[i] = str[i - 1] + 1;
+        i++;
+    }
+    str[i] = 0;
+
+    printf("%s\n",str);
+    res = ft_strrev(str);
+    printf("%s\n",res);
 }
